fe426c7c3af8a78c945b13689ca68ff8424c152e
Tanisha Negi <negitanisha77@gmail.com>
2026-04-28 10:29:36 +0530

    added backend dependencies

--- task-manager/server.js
+++ task-manager/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -45,6 +45,10 @@ app.delete("/tasks/:id", (req, res) => {
   res.json(deleted);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+
+
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
